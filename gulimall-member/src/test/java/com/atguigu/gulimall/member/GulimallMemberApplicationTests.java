17739e67069d5c2a2a0dd4b253878f200f468df6
hudi <huxiaodilw@capinfo.com.cn>
2024-02-14 21:10:08 +0800

    优化pom结构

--- gulimall-member/src/test/java/com/atguigu/gulimall/member/GulimallMemberApplicationTests.java
+++ gulimall-member/src/test/java/com/atguigu/gulimall/member/GulimallMemberApplicationTests.java
@@ -1,12 +1,7 @@
 package com.atguigu.gulimall.member;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class GulimallMemberApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
